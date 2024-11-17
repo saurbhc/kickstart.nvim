e3637f17dc413df421c5a73ed3d692e9df39429b
Saurabh Chopra <sc@saurabhchopra.co.uk>
2024-11-17 19:41:43 +0000

    add github/copilot.vim lazy plugin

--- init.lua
+++ init.lua
@@ -229,6 +229,7 @@ vim.opt.rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
+  'github/copilot.vim',
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
